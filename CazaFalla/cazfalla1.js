76dec8af2e42dfd0ec605be0519f73670c65cd84
Jose Monsalve <00jsmonsalve@gmail.com>
2025-06-17 08:38:29 -0500

    Cazafalla1 Solucionado

--- CazaFalla/cazfalla1.js
+++ CazaFalla/cazfalla1.js
@@ -1,20 +1,21 @@
-
 let cantidad = "25";
 let precio = 10;
 
-
-let total = cantidad + precio; 
+let total = cantidad * precio; 
 console.log("El total es: " + total); 
 console.log("El tipo de 'total' es: " + typeof total); 
+
 
 let cadenaNumerica = "123.45";
 let numeroEntero = parseInt(cadenaNumerica); 
 console.log("Número entero parseado: " + numeroEntero);
 
+
 let valorBooleano = Boolean(null); // Error 3: ¿Qué resultado esperas al convertir 'null' a booleano?
 console.log("Valor booleano de null: " + valorBooleano);
 
-let textoNumerico = "50";let resultadoMultiplicacion = textoNumerico * 2;
+let textoNumerico = "50";
+let resultadoMultiplicacion = textoNumerico * 2;
 console.log("Resultado de la multiplicación: " + resultadoMultiplicacion);
 console.log("El tipo de 'resultadoMultiplicacion' es: " + typeof resultadoMultiplicacion);
 
@@ -22,19 +23,21 @@ console.log("El tipo de 'resultadoMultiplicacion' es: " + typeof resultadoMultip
 function saludar(nombre) {
   console.log("Hola, " + nombre + "!");
 }
+let nombre = "Carlos";
+saludar(nombre);
 
-saludar();
 
 function calcularAreaRectangulo(largo, ancho) {
   return largo * ancho;
 }
-
-let area = calcularAreaRectangulo(5); 
+let area = calcularAreaRectangulo(5, 10); 
 console.log("El área del rectángulo es: " + area);
+
+
 
 function mostrarMensaje() {
   console.log("Este es un mensaje.");
-  return; 
+  return "Mensaje mostrado"; 
   console.log("Este código nunca se ejecutará."); 
 }
 
@@ -42,6 +45,9 @@ let mensajeRetornado = mostrarMensaje();
 console.log("Lo que retorna mostrarMensaje: " + mensajeRetornado);
 
 
-miFuncion(a, b) {
+
+function miFuncion(a, b){
     return a + b;
 }
+miSuma = miFuncion(5, 10);
+console.log("El resultado de la suma es: " + miSuma);
