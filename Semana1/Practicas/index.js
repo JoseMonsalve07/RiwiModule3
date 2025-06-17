d807fd71d9c9a835af793e12e6e432bbe71fb2bd
Jose Monsalve <00jsmonsalve@gmail.com>
2025-06-17 07:59:50 -0500

    Ejemplos

--- Semana1/Practicas/index.js
+++ Semana1/Practicas/index.js
@@ -1,1 +1,157 @@
-console.log("Hola mundo");
+// console.log("Hola mundo");
+
+
+
+
+
+// let numero = 10;
+// let texto = "El valor es: "
+
+
+
+
+
+// console.log(texto);
+// console.log(typeof texto);
+
+
+
+
+
+// let num = 123;
+// console.log(String(num));
+
+
+
+
+
+// console.log(parseInt("123"));
+// console.log(parseFloat("123.45"))
+
+
+
+
+
+// console.log(Boolean(1));
+// console.log(Boolean(0));
+// console.log(Boolean("Hola"));
+// console.log(Boolean(""));
+
+
+
+
+
+// function saludar(){
+//     console.log("Hola Mundo")
+// }
+// saludar();
+
+
+
+
+
+// function sumar(num1, num2){
+//     let resultado = num1 + num2;
+//     return resultado;
+// }
+// sumar(10, 5);
+// let miSuma = sumar(10, 5);
+// console.log("El resultado de mi suma es: " + miSuma);
+
+
+
+
+
+// let edad = 17;
+// if (edad>=18){
+//     console.log("Eres mayor de edad, puedes pasar.");
+// }
+// else if (edad>=16){
+//     console.log("Eres casi mayor de edad, puedes pasar con un adulto.");
+// }
+// else{
+//     console.log("Eres menor de edad, no puedes pasar.")
+// }
+
+
+
+
+// const edad = 20;
+// const puedeBeber = edad >= 18 ? "Sí, puede beber alcohol." : "No, aún no puede beber alcohol";
+// console.log(puedeBeber);
+
+
+
+
+
+// let diaSemana = "Sabado";
+// let mensaje;
+// switch (diaSemana){
+//     case "Lunes":
+//         mensaje = "Inicio de semana laboral.";
+//         break;
+//     case "Viernes":
+//         mensaje = "¡Casi fin de semana!";
+//         break;
+//     case "Sabado":
+//     case "Domingo":
+//         mensaje = "Es fin de semana";
+//         break;
+//     default:
+//         mensaje = "Semana";
+//     }
+// console.log(mensaje);
+
+
+
+
+
+// for (let i = 0;  i < 5; i++){
+//     console.log("Repetición número: " + i);
+// }
+
+
+
+
+
+// let contador = 0;
+// while (contador < 3){
+//     console.log("El contador es: " + contador);
+//     contador++;
+// }
+
+
+
+
+
+// let i = 0;
+// do{
+//     console.log("Valor de i: " + i);
+//     i++;
+// }
+// while (i<0);
+
+
+
+
+
+// const persona = {
+//     nombre: "Elena",
+//     edad: 28,
+//     ciudad: "Bogotá"
+// };
+// for (let propiedad in persona){
+//     console.log(`${propiedad}: ${persona[propiedad]}`)
+// }
+
+
+
+
+
+// const frutas = ["manzana", "banana", "cereza"];
+// for(let fruta of frutas){
+//     console.log(`Me gusta la ${fruta}.`);
+// }
+// for (let letra of "Hola"){
+//     console.log(letra);
+// }
