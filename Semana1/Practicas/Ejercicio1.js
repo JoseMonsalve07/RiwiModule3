0323fe485144da302f1137ba4da34b254c8ba490
Jose Monsalve <00jsmonsalve@gmail.com>
2025-06-17 12:02:50 -0500

    Ejercicios_Sesion1

---
+++ Semana1/Practicas/Ejercicio1.js
@@ -0,0 +1,24 @@
+//  EJERCICIO 1
+
+// 1. Gestionar el nombre de una empresa
+let nombre = "Google";
+console.log(`${nombre}\n`);
+
+// 2. Contar productos en inventario
+const cantidadProductos = 150;
+console.log(`${cantidadProductos}\n`);
+console.log(typeof cantidadProductos);
+
+// 3. Contar productos en inventario
+const precioBase = 29.99;
+console.log(`${precioBase}\n`);
+console.log(typeof precioBase);
+
+// 4. Indicar el estado de un recurso
+let estado = Boolean("");
+console.log(`${estado}\n`);
+
+// 5. Actualizar una puntuación
+let puntuacion = 100;
+puntuacion = 120;
+console.log(puntuacion)
