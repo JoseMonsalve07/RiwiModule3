b958223b0297ed07530bcfd2d293771c8f43eeae
Jose Monsalve <00jsmonsalve@gmail.com>
2025-06-17 12:22:20 -0500

    Ejercicios_Sesion1

---
+++ Semana1/Practicas/Ejercicio2.js
@@ -0,0 +1,29 @@
+//  EJERCICIO 2
+
+// 1. Verificar el tipo de un código postal
+let postal = "05001";
+console.log(postal)
+console.log(typeof postal)
+
+// 2. Confirmar el tipo de un conteo de empleados
+let numberOfEmployees = 50;
+console.log(numberOfEmployees);
+console.log(typeof numberOfEmployees)
+
+// 3. Asegurar el tipo de un porcentaje
+let IVA = 0.19;
+console.log(IVA);
+console.log(typeof IVA);
+
+// 4. Comprobar el estado de disponibilidad
+let value = false;
+console.log(value);
+console.log(typeof value);
+
+// 5. Clasificar información variada
+let animal = "perro";
+let legsNumber = 4;
+let itsMammalian = true;
+console.log(typeof animal);
+console.log(typeof legsNumber);
+console.log(typeof itsMammalian);
