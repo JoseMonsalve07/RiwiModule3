6ab84187648081eb0095ee8e7f64c5852e2965a5
Jose Monsalve <00jsmonsalve@gmail.com>
2025-06-18 11:06:21 -0500

    Ejercicios 2 Sesion 1

---
+++ Semana1/Practicas/Ejercicio7_2.js
@@ -0,0 +1,10 @@
+// Parte 3: Combinación de Operaciones y Concatenación Compleja
+
+// Historia de Usuario 7: Monitoreo de Estado de Servidor
+
+let servidor = "Web_Server_01";
+let status = "Activo";
+let pingMs = 25;
+let mensajeEstado = `El servidor ${servidor} está ${status} con una latencia de ${pingMs}ms`;
+
+console.log(mensajeEstado);
