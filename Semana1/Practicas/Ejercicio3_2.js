030b4ce1da01ec7edeb71e9259c9596581509b07
Jose Monsalve <00jsmonsalve@gmail.com>
2025-06-18 08:26:00 -0500

    Ejercicios 2 Sesion 1

---
+++ Semana1/Practicas/Ejercicio3_2.js
@@ -0,0 +1,10 @@
+// Parte 2: Manipulación Avanzada de Cadenas y Datos Numéricos
+
+// Historia de Usuario 3: Detalles de Nomenclatura de Producto
+
+let nombreProducto = "Smartphone XYZ 128GB";
+longitudNombre = nombreProducto.length;
+primerCaracter = nombreProducto.charAt(0);
+ultimoCaracter = nombreProducto.charAt(longitudNombre - 1);
+
+console.log(`Longitud del nombre: ${longitudNombre}\nPrimer caracter: ${primerCaracter}\nÚltimo caracter: ${ultimoCaracter}`);
