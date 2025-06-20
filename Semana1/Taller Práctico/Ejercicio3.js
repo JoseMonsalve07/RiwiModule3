c8213f85bf06a5bb533fd723c11978b685e56899
Jose Monsalve <00jsmonsalve@gmail.com>
2025-06-20 09:06:19 -0500

    Ejercicios taller practico

--- Semana1/Taller Práctico/Ejercicio3.js
+++ Semana1/Taller Práctico/Ejercicio3.js
@@ -3,5 +3,15 @@
 
 let number;
 do {
-    number = parseInt(prompt("Inresa un número"));
+    number = parseInt(prompt("Ingresa un número"));
 } while (isNaN(number) || number <= 0);
+
+if ((number % 3 == 0) && (number % 5 == 0)) {
+    alert(`El número ${number} es divisible entre 3 y 5`);
+} else if (number % 3 == 0) {
+    alert(`El número ${number} es divisible entre 3`);
+} else if (number % 5 == 0) {
+    alert(`El número ${number} es divisible entre 5`);
+} else {
+    alert(`El número ${number} no es divisible por ninguno`)
+}
