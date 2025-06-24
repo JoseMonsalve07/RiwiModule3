d0b4fc4f36ef5832ab8257d1f57c052727ee9ee5
JoseMonsalve07 <00jsmonsalve@gmail.com>
2025-06-24 09:51:09 -0500

    Delete Semana2 directory

--- Semana2/script.js
+++
@@ -1,9 +0,0 @@
-let boton = document.getElementById("boton");
-
-boton.addEventListener("click",saludar);
-
-function saludar() {
-    let resultado = document.getElementById("resultado");
-
-    resultado.innerHTML += "<p>Hola mundo</p>"
-}
