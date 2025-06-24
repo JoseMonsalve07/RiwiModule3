34041d73f14473c3bc31fba7278041a83444e39a
Jose Monsalve <00jsmonsalve@gmail.com>
2025-06-24 08:30:47 -0500

    Semana 2

---
+++ Semana2/script.js
@@ -0,0 +1,9 @@
+let boton = document.getElementById("boton");
+
+boton.addEventListener("click",saludar);
+
+function saludar() {
+    let resultado = document.getElementById("resultado");
+
+    resultado.innerHTML += "<p>Hola mundo</p>"
+}
