53e16717affdf98858306afaffe097f58af30ea3
Jose Monsalve <00jsmonsalve@gmail.com>
2025-06-24 09:51:57 -0500

    Semana 2

---
+++ Semana2/Practicas/index.js
@@ -0,0 +1,57 @@
+// console.group("Author details");
+// console.log("Name: Jose");
+// console.log("Year: 2025");
+// console.groupEnd();
+
+
+
+
+
+// let persona = {
+//     nombre: 'Jose',
+//     edad: 30,
+//     saludar: function() {
+//         console.log("Hola, soy " + this.nombre);
+//     }
+// };
+
+// console.log(persona.nombre);
+// console.log(persona["edad"])
+// persona.saludar();
+
+// Object.keys(persona);
+// Object.values(persona);
+// Object.entries(persona);
+
+
+
+
+// Clousure
+// function oficina() {
+//     let laptop = "Dell XPS";
+//     let claveWifi = "oficina123";
+
+//     return function seguirTrabajando() {
+//         console.log("Trabajando con " + laptop + " y clave " + claveWifi);
+//     };
+// }
+
+// const pedro = oficina();
+// pedro();
+
+
+
+
+
+
+//Callback
+// function saludar(callback) { 
+//     console.log("Hola");
+//     callback();
+// }
+
+// function decirAdios() {
+//     console.log("Adiós");
+// }
+
+// saludar(decirAdios);
