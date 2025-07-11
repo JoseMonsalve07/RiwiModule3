d1c6daedb9a89f827d70bb78fae6aa7d0415edc8
Jose Monsalve <00jsmonsalve@gmail.com>
2025-07-11 06:30:28 -0500

    Commit

---
+++ async-await/n1.js
@@ -0,0 +1,31 @@
+const datos = [
+    {
+        id: 1,
+        title: 'Iron Man',
+        year: 2008
+    },
+    {
+        id: 2,
+        title: 'Spiderman: Homecoming',
+        year: 2017
+    },
+    {
+        id: 3,
+        title: 'Avengers: Engame',
+        year: 2019
+    }
+];
+
+// const getDatos =  () => {
+//     return datos;
+// }
+
+const getDatos =  () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        resolve(datos);
+    }, 1500);
+    });
+}
+
+getDatos().then((datos) => console.log(datos));
